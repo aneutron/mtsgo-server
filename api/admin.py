e889d3929739e2e668845b804b0f617fbc94a777
Ayoub Boudhar <a.boudhar@outlook.com>
2017-06-26 14:41:00 +0200

    Add admin interfaces with proper fields listed.

--- api/admin.py
+++ api/admin.py
@@ -1,4 +1,27 @@
 from django.contrib import admin
+from .models import *
 # -*- coding: utf8 -*-
+
+@admin.register(Spot)
+class SpotAdmin(admin.ModelAdmin):
+    list_display = ('centrex', 'centrey', 'centrez', 'current_question', 'startTime')
+
+    def current_question(self, obj):
+        return obj.currentQuestion.questionText
+
+    current_question.short_description = 'Question active'
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('questionText', 'difficulty', 'topic', 'score')
+
+@admin.register(ExclusionZone)
+class ExclusionZoneAdmin(admin.ModelAdmin):
+    list_fields = ('name', 'number_of_summits')
+
+    def number_of_summits(self, obj):
+        return(len(json_decode(obj.points)))
+    number_of_summits.short_description = 'Nb. de sommets'
+
 
 # Register your models here.
